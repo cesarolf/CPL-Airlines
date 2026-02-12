d43950b2a9dcf99d45149105ce73a2483fb9c490
Pedro Lukas <pedrolukasalves.ti@gmail.com>
2026-02-11 21:34:59 -0300

    Mudanças no sistema de cancelamento de passagem

--- funcoes.h
+++ funcoes.h
@@ -5,16 +5,16 @@
 #define TOTAL_VOOS 10
 
 typedef struct Passagem {
-    char nomePassageiro[50]; // Acho que a passagem n precisa do nome do passageiro não, vários
-    double preco;            // passageiros podem ter uma passagem igual
+    char nomePassageiro[50]; 
+    double preco;            
     int idVoo;
     int numAssento;
     struct Passagem *prox;
 } Passagem;
 
-typedef struct Assento { // será uma lista estática
+typedef struct Assento { 
     int numero;
-    int ocupado; // 0 para livre e 1 para ocupado
+    int ocupado; 
 } Assento;
 
 typedef struct Voo {
@@ -22,6 +22,7 @@ typedef struct Voo {
     char destino[50];
     char origem[50];
     int qtdAssentosOcupados;
+    double preco;
     Assento listaAssentos[TOTAL_ASSENTOS];
 } Voo;
 
@@ -29,7 +30,6 @@ typedef struct Passageiro {
     int id;
     char nome[50];
     int idade;
-    char sexo;
     Passagem *passagens;
     struct Passageiro *prox;
 } Passageiro;
@@ -62,13 +62,13 @@ void menuCompra(Voo *voos, Passageiro *listaPassageiros, Passagem **listaPassage
 
 void menuCadastro( Passageiro ** passageiros );
 
-void menuCancelamento( Voo *voos, Passagem **passagens );
+void menuCancelamento( Voo *voos, Passageiro *listaPassageiros );
 
 void menuVisualizacao(Voo *voos, Passagem *passagens, Passageiro *listaPassageiros);
 
 Passagem* criarPassagem( Passageiro *passageiros, Voo *voo, int numAssento );
 
-Passagem* cancelarPassagem( Passagem *lista, Voo *voo, int numAssento );
+void removerPassagemPorAssento(Passageiro *listaPassageiros, int idVoo, int numAssento);
 
 void listarPassagens(Passagem *lista);
 
